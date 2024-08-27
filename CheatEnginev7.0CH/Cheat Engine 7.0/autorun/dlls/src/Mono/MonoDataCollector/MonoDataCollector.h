9765d352b9bba91a1e274e9f3e740950add9ce76
richard233n <1943126663@qq.com>
2024-08-27 12:35:29 +0800

    last update

--- CheatEnginev7.0CH/Cheat Engine 7.0/autorun/dlls/src/Mono/MonoDataCollector/MonoDataCollector.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-extern HANDLE DataCollectorThread;
-extern HANDLE SuicideThread;
-extern HINSTANCE g_hInstance;
-DWORD WINAPI DataCollectorEntry(LPVOID lpThreadParameter);
-DWORD WINAPI SuicideCheck(LPVOID lpThreadParameter);
